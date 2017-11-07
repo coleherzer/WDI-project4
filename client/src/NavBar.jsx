7f30d440211b3657723639ec5bbcbd803999e21e
Cole Herzer <cole.herzer@gmail.com>
2017-11-07 10:50:30 -0800

    added sass, tried to get foundation styling to work correctly on navbar

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Colors } from 'react-foundation'
+import { ButtonGroup, Link as Flink } from 'react-foundation'
 
 const NavBar = (props) => {
 	return (
 		<div className='NavBar'>
-			<Link to="/">Home</Link>
 			{props.currentUser
 				? (
 					<span>
-						<Link to="/vip">VIP</Link>
-						<Link to="/logout">Log Out</Link>
+						<ButtonGroup>
+							<Link to="/">Home</Link>
+							<Link to="/logout">Log Out</Link>
+						</ButtonGroup>
 					</span>
 				)
 				: (
 					<span>
-					<Button color={Colors.SUCCESS}>Test</Button>
-						<Link to="/login">Log In</Link>
-						<Link to="/signup">Sign Up</Link>
+						<ButtonGroup>
+							<Link to="/">Home</Link>
+							<Link to="/login">Log In</Link>
+							<Link to="/signup">Sign Up</Link>
+						</ButtonGroup>
 					</span>
 				)
 			}
