47d9fbb6a389c1f2fa783ca02405b186076c98c4
Cole Herzer <cole.herzer@gmail.com>
2017-11-07 10:02:12 -0800

    finished crud for comments, added comments controller and appropriate routes to rants controller

---
+++ controllers/comments.js
@@ -0,0 +1,39 @@
+const Rant = require('../models/Rant.js')
+
+module.exports = {
+    // create a new comment
+	create: (req, res) => {
+        const newCommentData = {...req.body, user: req.user._id}
+        Rant.findById(req.params.id, (err, rant) => {
+            rant.comments.push(newCommentData)
+            rant.save((err, updatedRant) => {
+                res.json({success: true, message: 'Comment added', rant: updatedRant})
+            })
+        })
+    },
+    destroy: (req, res) => {
+        Rant.findById(req.params.rantId, (err, rant) => {
+            var commentsList = rant.comments
+            var index
+            for(let i = 0; i < commentsList.length; i++) {
+                if(commentsList[i]._id == req.params.commentId) {
+                    index = i
+                    break
+                }
+            }
+            commentsList.splice(index, 1)
+            rant.save((err, updatedRant) => {
+                res.json({success: true, message: "Comment deleted", rant: updatedRant})
+            })
+        })
+    },
+    update: (req, res) => {
+        Rant.findById(req.params.rantId, (err, rant) => {
+            var comment = rant.comments.id(req.params.commentId)
+            Object.assign(comment, req.body)
+            rant.save((err, updatedRant) => {
+                res.json({message: "comment updated", success: true, rant: updatedRant})
+            })
+        })
+    }
+}
--- routes/rants.js
+++ routes/rants.js
@@ -1,7 +1,8 @@
 const
     express = require('express'),
     rantsRouter = new express.Router(),
-    rantsCtrl = require('../controllers/rants.js')
+    rantsCtrl = require('../controllers/rants.js'),
+    commentsCtrl = require('../controllers/comments.js'),
     verifyToken = require('../serverAuth.js').verifyToken
 
 rantsRouter.use(verifyToken)
@@ -14,6 +15,13 @@ rantsRouter.route('/:id')
     .get(rantsCtrl.show)
     .patch(rantsCtrl.update)
     .delete(rantsCtrl.destroy)
+
+rantsRouter.route('/:id/comments')
+    .post(commentsCtrl.create)
+
+rantsRouter.route('/:rantId/comments/:commentId')
+    .delete(commentsCtrl.destroy)
+    .patch(commentsCtrl.update)
 
 module.exports = rantsRouter
 
